e7c952158ab9781c0c74d6c13a9f1ff4605e1fcc
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-11-24 12:58:18 +0200

    Fix Greetings Repository

--- src/main/java/com/emanuelciuca/training/repo/GreetingsRepository.java
+++ src/main/java/com/emanuelciuca/training/repo/GreetingsRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface GreetingsRepository extends JpaRepository<Long, Greeting> {
+public interface GreetingsRepository extends JpaRepository<Greeting, Long> {
 
     Optional<Greeting> findByContent(String content);
 }
