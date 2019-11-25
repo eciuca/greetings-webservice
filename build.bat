8764b185e7d671942925d451f2b0ddc3b2ed4cb3
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-11-25 16:22:22 +0200

    Add run scripts with docker

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
+@echo off
+
 docker run --name greetings-ws-build-container ^
  -it --rm ^
- -v %cd%:/usr/src/mymaven ^
+ -v "%cd%"":/usr/src/mymaven ^
  -w /usr/src/mymaven ^
  maven:3.6-jdk-13 ^
  mvn clean package
---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+
+docker run --name greetings-ws-build-container ^
+ --rm ^
+ -p 8080:8080 ^
+ -v "%pwd%/target":/usr/src/myapp ^
+ -w /usr/src/myapp ^
+ openjdk:13 ^
+ java -jar greetings-webservice-0.0.1-SNAPSHOT.jar
