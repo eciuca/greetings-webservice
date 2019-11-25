a82d7a91b331241a851960b531b2529596daa495
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-11-25 16:27:01 +0200

    Fix batch script paths

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 docker run --name greetings-ws-build-container ^
  -it --rm ^
- -v "%cd%"":/usr/src/mymaven ^
+ -v "%cd%":/usr/src/mymaven ^
  -w /usr/src/mymaven ^
  maven:3.6-jdk-13 ^
  mvn clean package
